d458e0d1175f0a6edebc5c4493a6688c694e8402
Dumitru <119630796+DiacencoDumitru@users.noreply.github.com>
2024-12-12 14:22:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#### Напишем первое Hibernate приложение.
+1. hibernate.properties (Data Source БД, Hibernate Configuration) 
+2. Entity сопостовление сущности с данными БД.
+3. SessionFactory, Transaction (полученим строку человека с id = 1, его: имя, возраст)
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+// Unit test
+public class AppTest 
+{
+    // Строгий тест (Rigorous Test)
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
